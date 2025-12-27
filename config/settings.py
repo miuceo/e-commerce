823cc551706a414b95c9ac498d3a0ef5cc036c68
Muhammadjon Ibrohimov <ibrokhimovmiu@gmail.com>
2025-12-27 14:26:00 +0500

    added

--- config/settings.py
+++ config/settings.py
@@ -162,10 +162,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = "/var/www/ecommerce/staticfiles/"
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = "/var/www/ecommerce/media/"
 
 AUTH_USER_MODEL = "authentication.CustomUser"
 
